c1348095100e2a9830590a8d43f6a1db347755e6
cqxmzhc <ldzslds@gmail.com>
2017-04-06 18:09:25 +0800

    bst delete

--- tree/bst/binary_search_tree.go
+++ tree/bst/binary_search_tree.go
@@ -43,13 +43,49 @@ func Insert(root *node, value int) bool {
 	}
 }
 
+//Delete delete value from tree
+func Delete(root *node, value int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.value == value {
+		if root.leftChild == nil && root.rightChild == nil {
+			root = nil
+		}
+
+		if root.leftChild == nil && root.rightChild != nil {
+			root.value = root.rightChild.value
+			root.leftChild = root.rightChild.leftChild
+			root.rightChild = root.rightChild.rightChild
+		}
+
+		if root.leftChild != nil && root.rightChild == nil {
+			root.value = root.leftChild.value
+			root.leftChild = root.leftChild.leftChild
+			root.rightChild = root.leftChild.rightChild
+		}
+	} else if root.value > value {
+		Delete(root.leftChild, value)
+	} else {
+		Delete(root.rightChild, value)
+	}
+
+}
+
 //PrintBST print bst
 func PrintBST(root *node) {
 	if root == nil {
 		return
 	}
 
+	if root.leftChild != nil {
+		PrintBST(root.leftChild)
+	}
+
 	fmt.Println(root.value)
-	fmt.Println(root.leftChild)
-	fmt.Println(root.rightChild)
+
+	if root.rightChild != nil {
+		PrintBST(root.rightChild)
+	}
 }
