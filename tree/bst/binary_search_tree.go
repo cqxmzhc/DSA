c9a1d5baf65656514ab3a2abd95f6b315f7bc675
cqxmzhc <ldzslds@gmail.com>
2017-04-07 07:04:29 +0800

    bst find value

--- tree/bst/binary_search_tree.go
+++ tree/bst/binary_search_tree.go
@@ -52,25 +52,46 @@ func Delete(root *node, value int) bool {
 	if root.value == value {
 		if root.leftChild == nil && root.rightChild == nil {
 			root = nil
+			return true
 		}
 
 		if root.leftChild == nil && root.rightChild != nil {
 			root.value = root.rightChild.value
 			root.leftChild = root.rightChild.leftChild
 			root.rightChild = root.rightChild.rightChild
+
+			return true
 		}
 
 		if root.leftChild != nil && root.rightChild == nil {
 			root.value = root.leftChild.value
-			root.leftChild = root.leftChild.leftChild
 			root.rightChild = root.leftChild.rightChild
+			root.leftChild = root.leftChild.leftChild
+
+			return true
 		}
 	} else if root.value > value {
-		Delete(root.leftChild, value)
+		return Delete(root.leftChild, value)
 	} else {
-		Delete(root.rightChild, value)
+		return Delete(root.rightChild, value)
 	}
 
+	return false
+}
+
+//Find find value
+func Find(root *node, value int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.value == value {
+		return true
+	} else if root.value > value {
+		return Find(root.leftChild, value)
+	} else {
+		return Find(root.rightChild, value)
+	}
 }
 
 //PrintBST print bst
